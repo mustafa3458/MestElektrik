3927e2a163ffce4aebe0ed74bf98855b5a6f4a08
mustafa3458 <86317116+mustafa3458@users.noreply.github.com>
2026-04-04 23:28:28 +0300

    fix: remove static export config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: { unoptimized: true },
-  basePath: "/MestElektrik",
-  assetPrefix: "/MestElektrik/",
 };
 
 export default nextConfig;
